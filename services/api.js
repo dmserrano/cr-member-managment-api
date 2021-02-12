9f18011d60a6057422655ce8b020d8e42b44d60c
Dominic Serrano <dserrano@atiba.com>
2021-02-12 15:58:26 -0600

    Added fixie proxy onto the request lib for production requests so that all requests are from a static set of IP addresses

--- services/api.js
+++ services/api.js
@@ -1,7 +1,12 @@
 const request = require("request-promise");
 
+const { FIXIE_URL, NODE_ENV } = process.env;
+
+const fixieRequest = request.defaults({ proxy: FIXIE_URL });
+const requestMethod = NODE_ENV === "production" ? fixieRequest : request;
+
 const getRequest = async (url = "", options = {}) => {
-    const response = await request(url, options);
+    const response = await requestMethod(url, options);
     return JSON.parse(response);
 };
 
